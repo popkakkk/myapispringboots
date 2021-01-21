ef562b83f413864f7a46294a80ccbc7b94f17e9f
poppatipan39 <sir.poppatipan@gmail.com>
2021-01-21 22:27:37 +0700

    initial project

---
+++ src/main/java/com/example/myapi/demo/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.myapi.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/getWelcome")
+    public String getWelcome(){
+
+        return "Welcome ";
+    }
+
+
+}
---
+++ src/main/java/com/example/myapi/demo/MyapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.myapi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyapiApplication.class, args);
+    }
+
+}
